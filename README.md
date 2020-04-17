154d2095859c493cada7fff2e9e24d070c913022
sraj <santhoshvfsd@outlook.com>
2020-04-17 09:49:38 +0530

    error handling and get latitude details

--- README.md
+++ README.md
@@ -19,3 +19,13 @@ these are accessed by props.children
 ### semantic ui using CDN
 
 ### faker js to mock data
+
+### identiy a particular type of component from the children
+
+convert the children to React.Children.toArray()
+then filter it based on the child.type .name
+
+### Geolocatio
+
+window.navigator.location.getGeoLocation ->
+Resetting location -> CG+I -> Sensors Tab
--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from "react";
-import SeasonDisplay from "./SeasonDisplay";
+import React, { Component } from "react";
 
-const App = () => {
-  return <SeasonDisplay />;
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: null,
+      errorMessage: "",
+    };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setLatitude(position),
+      (err) => this.setErr(err)
+    );
+  }
 
+  setErr = (error) => {
+    this.setState({ errorMessage: error.message });
+  };
+
+  setLatitude = (position) => {
+    this.setState({ latitude: position.coords.latitude });
+  };
+
+  render() {
+    if (this.state.errorMessage && !this.state.latitude) {
+      console.log("error");
+      return <div>Error : {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.latitude) {
+      console.log("sucess");
+      return <div> Latitude : {this.state.latitude}</div>;
+    }
+
+    return <div>Loading !!!! </div>;
+  }
+}
 export default App;
