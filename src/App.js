5bd67ffb23ea1a3474e8f8b635d8af7189a9a1be
sraj <santhoshvfsd@outlook.com>
2020-04-17 08:13:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from "react";
-import CommentDetails from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
-import faker from "faker";
-const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <CommentDetails name="Sam" imageUrl={faker.image.avatar} />
-      </ApprovalCard>
+import SeasonDisplay from "./SeasonDisplay";
 
-      <ApprovalCard>
-        <CommentDetails name={"Ram"} imageUrl={faker.image.avatar} />
-      </ApprovalCard>
-    </div>
-  );
+const App = () => {
+  return <SeasonDisplay />;
 };
 
 export default App;
