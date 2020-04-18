f335704006be5d532bec09f9e8526c0dfde0f84a
sraj <santhoshvfsd@outlook.com>
2020-04-18 12:26:21 +0530

    customizing client

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
+import unsplash from "./Api/Unsplash";
 import SearchBar from "./Component/SearchBar";
 
 class App extends React.Component {
-  onSubmitSearch(term) {
-    console.log(term);
-  }
+  state = {
+    images: [],
+  };
+
+  onSubmitSearch = async (term) => {
+    const response = await unsplash.get(
+      "https://api.unsplash.com/search/photos",
+      {
+        params: { query: term },
+      }
+    );
+    this.setState({ images: response.data.results });
+    console.log(this.state.images);
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSearchSubmit={this.onSubmitSearch} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
